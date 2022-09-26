69eebcf1076810683e30fa33a083fcbc93c77128
iamsagnik <sagnikmitra008@gmail.com>
2022-09-26 17:43:55 +0530

    Area function added 🔁

--- main.py
+++ main.py
@@ -7,8 +7,15 @@ def circumf(r):
     c = 2 * p * r
     return c
 
+def area(r):
+    p = math.pi
+    area = p * r * r
+    return area
+
+
 r = float(input("Enter radius :"))
 print("C = ", circumf(r))
+print("Area =", area(r))
 
 
 
